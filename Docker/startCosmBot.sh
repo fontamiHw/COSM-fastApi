608fda976a06d7e6a01afeb37997f38f3a2e6280
MikHw <fontami1@gmail.com>
2024-12-28 12:04:36 +0100

    added both port on remapping

--- Docker/startCosmBot.sh
+++ Docker/startCosmBot.sh
@@ -7,9 +7,7 @@ yaml_file="${RESOURCE_PATH}/CosmFastapi-config.yaml"
 # Use yq to extract the value of 'port'
 FASTAPI_PORT=$(yq eval '.fastapi.port' "$yaml_file")
 FASTAPI_HOST=$(yq eval '.fastapi.host' "$yaml_file")
+CONTAINERCOMM_PORT=$(yq eval '.container_communication.port' "$yaml_file")
 
-echo "docker run --network Cosm-net --ip ${FASTAPI_HOST} -v /home/civico129/MyProject/COSM-fastApi/host:/app/host -e FASTAPI_HOST=${FASTAPI_HOST} -e FASTAPI_PORT=${FASTAPI_PORT} -p 8000:8000 --name CosmFastApi cosm-fastapi"
-      docker run --network Cosm-net --ip ${FASTAPI_HOST} -v /home/civico129/MyProject/COSM-fastApi/host:/app/host -e FASTAPI_HOST=${FASTAPI_HOST} -e FASTAPI_PORT=${FASTAPI_PORT} -p 8000:8000 --name CosmFastApi cosm-fastapi
-
-# echo 'docker run -v /home/civico129/MyProject/COSM-fastApi/host:/app/host -e FASTAPI_HOST="127.0.0.1" -e FASTAPI_PORT="8000" --name CosmFastApi cosm-fastapi'
-# docker run -v /home/civico129/MyProject/COSM-fastApi/host:/app/host -e FASTAPI_HOST="192.168.1.75" -e FASTAPI_PORT="8000" --name CosmFastApi cosm-fastapi
+echo "docker run --network Cosm-net --ip ${FASTAPI_HOST} -v /home/civico129/MyProject/COSM-fastApi/host:/app/host -e FASTAPI_HOST=${FASTAPI_HOST} -e FASTAPI_PORT=${FASTAPI_PORT} -p ${CONTAINERCOMM_PORT}:${CONTAINERCOMM_PORT} -p ${FASTAPI_HOST}:${FASTAPI_HOST} --name CosmFastApi cosm-fastapi"
+      docker run --network Cosm-net --ip ${FASTAPI_HOST} -v /home/civico129/MyProject/COSM-fastApi/host:/app/host -e FASTAPI_HOST=${FASTAPI_HOST} -e FASTAPI_PORT=${FASTAPI_PORT} -p ${CONTAINERCOMM_PORT}:${CONTAINERCOMM_PORT} -p ${FASTAPI_HOST}:${FASTAPI_HOST} --name CosmFastApi cosm-fastapi
